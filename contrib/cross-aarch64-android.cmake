0b02878bb11b448922d4a2e2a8c71514f57ef150
xuchengpu mac <1550540124@qq.com>
2025-09-17 11:09:48 +0800

    x86

--- contrib/cross-aarch64-android.cmake
+++ contrib/cross-aarch64-android.cmake
@@ -1,5 +1,5 @@
 #
-# CMake Toolchain file for crosscompiling Android / aarch64
+# CMake Toolchain file for crosscompiling Android / x86
 #
 # This can be used when running cmake in the following way:
 #  cd build/
@@ -8,8 +8,8 @@
 
 
 set(ANDROID_API_VER 24)
-set(ABARCH1 arm)
-set(CMAKE_SYSTEM_PROCESSOR armv7a)
+set(ABARCH1 x86)
+set(CMAKE_SYSTEM_PROCESSOR i686)
 set(NDK /Users/xuchengpu/Library/Android/sdk/ndk/21.1.6352462/)
 set(CROSS_SYSROOT "${NDK}/platforms/android-${ANDROID_API_VER}/arch-${ABARCH1}")
 set(BUILD_ARCH darwin-x86_64)
@@ -18,7 +18,7 @@ set(BUILD_ARCH darwin-x86_64)
 # Rest should be computed from the above
 #
 set(TC_PATH ${NDK}/toolchains/llvm/prebuilt/${BUILD_ARCH})
-set(TC_BASE ${TC_PATH}/bin/${CMAKE_SYSTEM_PROCESSOR}-linux-androideabi)
+set(TC_BASE ${TC_PATH}/bin/${CMAKE_SYSTEM_PROCESSOR}-linux-android)
 set(PLATFORM android)
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_C_COMPILER "${TC_BASE}${ANDROID_API_VER}-clang")
@@ -42,7 +42,7 @@ if (CMAKE_BUILD_TYPE MATCHES RELEASE OR CMAKE_BUILD_TYPE MATCHES Release OR CMAK
 endif()
 
 #-nostdlib
-SET(CMAKE_C_FLAGS "-DGCC_VER=\"\\\"$(GCC_VER)\\\"\" -Wno-error=shorten-64-to-32 -Wno-error=sign-conversion -Wno-error=sign-compare -DARM=1 -Os -g3 -fpie -fPIC -ffunction-sections -fdata-sections -D__ANDROID_API__=${ANDROID_API_VER} -Wno-pointer-sign" CACHE STRING "" FORCE)
+SET(CMAKE_C_FLAGS "-DGCC_VER=\"\\\"$(GCC_VER)\\\"\" -Wno-error=shorten-64-to-32 -Wno-error=sign-conversion -Wno-error=sign-compare -DX86=1 -Os -g3 -fpie -fPIC -ffunction-sections -fdata-sections -D__ANDROID_API__=${ANDROID_API_VER} -Wno-pointer-sign" CACHE STRING "" FORCE)
 
 set(CMAKE_FIND_ROOT_PATH "${CROSS_SYSROOT}")
 
@@ -53,5 +53,3 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # Search headers and libraries in the target environment only.
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-
