8fe03b0ccdf20e92f7828855ff38261a829eb739
xuchengpu mac <1550540124@qq.com>
2025-09-23 17:37:55 +0800

    添加压缩支持

--- scripts/build_with_libevent_boringssl.sh
+++ scripts/build_with_libevent_boringssl.sh
@@ -23,5 +23,7 @@ cmake .. -DCMAKE_TOOLCHAIN_FILE=../contrib/cross-aarch64-android.cmake \
 -DOPENSSL_INCLUDE_DIRS="${BORINGSSL_INCLUDE}" \
 -DOPENSSL_LIBRARIES="${ANDROID_LIBS_PATH}/libssl.a;${ANDROID_LIBS_PATH}/libcrypto.a" \
 -DLWS_WITHOUT_TESTAPPS=1 && \
+-DLWS_WITHOUT_EXTENSIONS=OFF \
+-DLWS_WITH_ZLIB=ON \
 make && \
 cmake --install .
